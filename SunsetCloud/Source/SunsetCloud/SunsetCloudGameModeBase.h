6e66f7c259689488f9902cc13ae1388e51d0ad77
jams10 <jamin3710@gmail.com>
2023-06-03 22:27:36 +0900

    001-2. Created the unreal project.

---
+++ SunsetCloud/Source/SunsetCloud/SunsetCloudGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SunsetCloudGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUNSETCLOUD_API ASunsetCloudGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
