7b9f369700f891766cb1bd0120e0221dd25aed96
jams10 <jamin3710@gmail.com>
2023-06-30 21:55:02 +0900

    011. Added GameplayAbility plugin to project, created ASC and AttributeSet class.

---
+++ SunsetCloud/Source/SunsetCloud/Public/AbilitySystem/SCAttributeSet.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "SCAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUNSETCLOUD_API USCAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
