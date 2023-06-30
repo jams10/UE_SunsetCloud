034af4d6098fa061d029fb2f0c261003da7ee31f
jams10 <jamin3710@gmail.com>
2023-06-30 20:19:07 +0900

    009. Changed character rotation options.

--- SunsetCloud/Source/SunsetCloud/Private/Character/SCPlayerCharacter.cpp
+++ SunsetCloud/Source/SunsetCloud/Private/Character/SCPlayerCharacter.cpp
@@ -9,4 +9,11 @@ ASCPlayerCharacter::ASCPlayerCharacter(const FObjectInitializer& ObjectInitializ
 {
 	// 캐릭터 무브먼트 컴포넌트를 우리가 만들어준 커스텀 CMC 타입으로 캐스팅하고, 나중에 사용하기 위해 참조를 따로 저장해줌.
 	SCCharacterMovementComponent = Cast<USCCharacterMovementComponent>(GetCharacterMovement());
+
+	GetCharacterMovement()->bOrientRotationToMovement = true; // 이동 방향으로 회전하도록 함.
+
+	// 컨트롤러 회전에 따라 캐릭터가 회전하지 않도록 함.
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
 }
